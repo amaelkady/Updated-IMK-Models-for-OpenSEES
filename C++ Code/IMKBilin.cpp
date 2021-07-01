3f98b9720fb1096c3e77efc6125b672af85b9784
Ahmed Elkady <a.elkady@soton.ac.uk>
2021-07-01 19:31:17 +0100

    IMKBilin update
    Fix zigzag bug in high freq un/reloading

--- C++ Code/IMKBilin.cpp
+++ C++ Code/IMKBilin.cpp
@@ -337,7 +337,8 @@ int IMKBilin::setTrialStrain(double strain, double strainRate)
 	}
 
 	//  Simple and unified notation for current bacbone parameters
-	if (Di >= 0.0) {
+	Mi_temp = Mi_1 + K_j * (Ri - Ri_1);
+	if (Mi_temp >= 0.0) {
 		Ki = K_j;
 		slope_pi = slope_p_pos_j;
 		slope_pci = slope_pc_pos_j;
