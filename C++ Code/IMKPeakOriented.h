284531dc564b5d77aa3aa93a77775492a1933540
Kazuki Ichinohe <76049354+z-ichinohe@users.noreply.github.com>
2022-07-18 22:22:03 +0900

    Update IMKPeakOriented.h

--- C++ Code/IMKPeakOriented.h
+++ C++ Code/IMKPeakOriented.h
@@ -57,140 +57,140 @@ public:
 protected:
 
 private:
-    //my functions
+	//my functions
 
-    //Fixed input material parameters
-    double Ke;
-    double Up_pos;
-    double Upc_pos;
-    double Uu_pos;
-    double Fy_pos;
-    double FcapFy_pos;
-    double ResF_pos;
-    double Up_neg;
-    double Upc_neg;
-    double Uu_neg;
-    double Fy_neg;
-    double FcapFy_neg;
-    double ResF_neg;
-    double LAMBDA_S;
-    double LAMBDA_C;
-    double LAMBDA_A;
-    double LAMBDA_K;
-    double c_S;
-    double c_C;
-    double c_A;
-    double c_K;
-    double D_pos;
-    double D_neg;
+	//Fixed input material parameters
+	double Ke;
+	double Up_pos;
+	double Upc_pos;
+	double Uu_pos;
+	double Fy_pos;
+	double FcapFy_pos;
+	double ResF_pos;
+	double Up_neg;
+	double Upc_neg;
+	double Uu_neg;
+	double Fy_neg;
+	double FcapFy_neg;
+	double ResF_neg;
+	double LAMBDA_S;
+	double LAMBDA_C;
+	double LAMBDA_A;
+	double LAMBDA_K;
+	double c_S;
+	double c_C;
+	double c_A;
+	double c_K;
+	double D_pos;
+	double D_neg;
 
-    //State variables 
-    double U, cU;
+	//State variables 
+	double U, cU;
 
-    //History variables
+	//History variables
 
-    double ui, 		cui;
-    double fi, 		cfi;
-    double ui_1,	cui_1;
-    double fi_1, 	cfi_1;
-    double du_i_1,	cdu_i_1;
+	double ui, 		cui;
+	double fi, 		cfi;
+	double ui_1,	cui_1;
+	double fi_1, 	cfi_1;
+	double du_i_1,	cdu_i_1;
 
-    double posUy_1,				cposUy_1;
-    double posUcap_1,			cposUcap_1;
-    double posFy_1,				cposFy_1;
-    double posFcap_1,			cposFcap_1;
-    double posUGlobal_1,		cposUGlobal_1;
-    double posFGlobal_1,		cposFGlobal_1;
-    double posUres_1,			cposUres_1;
-    double posFres_1,			cposFres_1;
-    double posKp_1,				cposKp_1;
-    double posKpc_1,			cposKpc_1;
+	double posUy_1,				cposUy_1;
+	double posUcap_1,			cposUcap_1;
+	double posFy_1,				cposFy_1;
+	double posFcap_1,			cposFcap_1;
+	double posUGlobal_1,		cposUGlobal_1;
+	double posFGlobal_1,		cposFGlobal_1;
+	double posUres_1,			cposUres_1;
+	double posFres_1,			cposFres_1;
+	double posKp_1,				cposKp_1;
+	double posKpc_1,			cposKpc_1;
 
 
-    double negUy_1,             cnegUy_1;
-    double negUcap_1,           cnegUcap_1;
-    double negFy_1,             cnegFy_1;
-    double negFcap_1,           cnegFcap_1;
-    double negUGlobal_1,        cnegUGlobal_1;
-    double negFGlobal_1,        cnegFGlobal_1;
+	double negUy_1,             cnegUy_1;
+	double negUcap_1,           cnegUcap_1;
+	double negFy_1,             cnegFy_1;
+	double negFcap_1,           cnegFcap_1;
+	double negUGlobal_1,        cnegUGlobal_1;
+	double negFGlobal_1,        cnegFGlobal_1;
 
-    double negUres_1,           cnegUres_1;
-    double negFres_1,           cnegFres_1;
-    double negKp_1,             cnegKp_1;
-    double negKpc_1,            cnegKpc_1;
+	double negUres_1,           cnegUres_1;
+	double negFres_1,           cnegFres_1;
+	double negKp_1,             cnegKp_1;
+	double negKpc_1,            cnegKpc_1;
 
-    double K_unload, cK_unload;
+	double K_unload, cK_unload;
 
-    double Energy_Acc,  cEnergy_Acc;
-    double Energy_Diss, cEnergy_Diss;
+	double Energy_Acc,  cEnergy_Acc;
+	double Energy_Diss, cEnergy_Diss;
 
-    double u0, cu0;
+	double u0, cu0;
 
-    double du;
-    double df;
+	double du;
+	double df;
 
-    bool   FailS;
-    bool   FailC;
-    bool   FailA;
-    bool   FailK;
-    bool   FailPp;
-    bool   FailPn;
-    bool   FailRp;
-    bool   FailRn;
-    bool   FailDp;
-    bool   FailDn;
+	bool   FailS;
+	bool   FailC;
+	bool   FailA;
+	bool   FailK;
+	bool   FailPp;
+	bool   FailPn;
+	bool   FailRp;
+	bool   FailRn;
+	bool   FailDp;
+	bool   FailDn;
 
-    double Ei;
-    double dEi;
-    double Epj;
-    double EpjK;
-    double EiK;
+	double Ei;
+	double dEi;
+	double Epj;
+	double EpjK;
+	double EiK;
 
-    double c_cS;
-    double c_cC;
-    double c_cA;
-    double c_cK;
+	double c_cS;
+	double c_cC;
+	double c_cA;
+	double c_cK;
 
-    double EtS;
-    double EtC;
-    double EtA;
-    double EtK;
+	double EtS;
+	double EtC;
+	double EtA;
+	double EtK;
 
-    double betaS;
-    double betaC;
-    double betaA;
-    double betaK;
+	double betaS;
+	double betaC;
+	double betaA;
+	double betaK;
 
-    double sPCsp,  sPCsn;
-    double sPCpcp, sPCpcn;
+	double sPCsp,  sPCsn;
+	double sPCpcp, sPCpcn;
 
-    double TangentK, cTangentK, ki;
+	double TangentK, cTangentK, ki;
 
-    double Uy_pos,		Uy_neg;
-    double Ucap_pos,	Ucap_neg;
-    double Fcap_pos,	Fcap_neg;
-    double Kpc_pos,		Kpc_neg;
-    double Kp_pos,		Kp_neg;
+	double Uy_pos,		Uy_neg;
+	double Ucap_pos,	Ucap_neg;
+	double Fcap_pos,	Fcap_neg;
+	double Kpc_pos,		Kpc_neg;
+	double Kp_pos,		Kp_neg;
 
-    double posULocal_1,		cposULocal_1;
-    double posFLocal_1,		cposFLocal_1;
-    double negULocal_1,		cnegULocal_1;
-    double negFLocal_1,		cnegFLocal_1;
+	double posULocal_1,		cposULocal_1;
+	double posFLocal_1,		cposFLocal_1;
+	double negULocal_1,		cnegULocal_1;
+	double negFLocal_1,		cnegFLocal_1;
 //
-    bool    Failure_Flag,	cFailure_Flag;
-    bool    Excursion_Flag,	cExcursion_Flag;
-    // bool Reloading_Flag,	cReloading_Flag;
-    // bool TargetPeak_Flag,cTargetPeak_Flag;
-    int     Branch,			cBranch;
-    bool    Yield_Flag,		cYield_Flag;
-    bool    Reversal_Flag,	cReversal_Flag;
-    int     exBranch,		cexBranch;
+	bool    Failure_Flag,	cFailure_Flag;
+	bool    Excursion_Flag,	cExcursion_Flag;
+	// bool Reloading_Flag,	cReloading_Flag;
+	// bool TargetPeak_Flag,cTargetPeak_Flag;
+	int     Branch,			cBranch;
+	bool    Yield_Flag,		cYield_Flag;
+	bool    Reversal_Flag,	cReversal_Flag;
+	int     exBranch,		cexBranch;
 //
-    double K_reload,       cK_reload;
+	double K_reload,       cK_reload;
 //
-    double K_Local;
-    double K_Global;
-    // double K_check;
+	double K_Local;
+	double K_Global;
+	// double K_check;
 
 
 };
